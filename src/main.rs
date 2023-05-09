c59e39bca2836d1e7bec5a7afe81683bf6ca1769
Brenek Harrison <brenekharrison@gmail.com>
2023-05-09 13:20:56 -0600

    Add user input for Midi controller port in tester program

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread, time};
+use std::{io, io::Write, thread, time};
 
 fn main() {
     let client_1 = etc_express_midi::MidiOutput::new("Client 1").unwrap();
@@ -10,10 +10,19 @@ fn main() {
     }
     println!("================================\n");
 
-    let mut midi_conn = client_1.connect(&(ports[0]), "Test").unwrap();
+    print!("Please enter the index of the target Midi controller: ");
+    io::stdout().flush().expect("Couldn't flush stdout");
+    let mut input_line = String::new();
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+    let midi_port_index: usize = input_line.trim().parse().expect("Input not an integer");
+    println!("");
+
+    let mut midi_conn = client_1.connect(&(ports[midi_port_index]), "Test").unwrap();
     let device_id = 16;
     println!("Sending Go command using Midi Show Control");
-    etc_express_midi::go_msc(&mut midi_conn, device_id, etc_express_midi::FaderPair::AB).unwrap();
+    etc_express_midi::go_msc(&mut midi_conn, device_id, etc_express_midi::FaderPair::CD).unwrap();
     println!("Sent first Go command");
 
     for i in (1..6).rev() {
@@ -26,7 +35,7 @@ fn main() {
     etc_express_midi::go_etc_midi(
         &mut midi_conn,
         midi_channel,
-        etc_express_midi::FaderPair::AB,
+        etc_express_midi::FaderPair::CD,
     )
     .unwrap();
     println!("Sent second Go command");
