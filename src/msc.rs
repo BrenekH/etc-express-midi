fa5ef60562f13e25925befa0020437db3a549275
Brenek Harrison <brenekharrison@gmail.com>
2023-05-09 19:55:36 -0600

    Finish off other MSC functions

--- src/msc.rs
+++ src/msc.rs
@@ -15,24 +15,70 @@ impl<'a> ConsoleMSC<'a> {
     }
 
     pub fn go(&mut self, fader_pair: FaderPair) -> Result<(), SendError> {
-        self.send_msc_frame(Command::Go, 0, fader_pair.value())
+        self.send_msc_frame(Command::Go, Some(fader_pair), Some(0))
     }
 
-    pub fn go_cue() {}
+    pub fn go_cue(&mut self, fader_pair: FaderPair, cue_number: u16) -> Result<(), SendError> {
+        self.send_msc_frame(Command::Go, Some(fader_pair), Some(cue_number))
+    }
 
-    pub fn stop() {}
+    pub fn stop(&mut self, fader_pair: FaderPair) -> Result<(), SendError> {
+        self.send_msc_frame(Command::Stop, Some(fader_pair), Some(0x69))
+    }
 
-    pub fn stop_all() {}
+    pub fn stop_all(&mut self) -> Result<(), SendError> {
+        self.send_msc_frame(Command::Stop, None, None)
+    }
 
-    pub fn resume() {}
+    pub fn resume(&mut self, fader_pair: FaderPair) -> Result<(), SendError> {
+        self.send_msc_frame(Command::Resume, Some(fader_pair), Some(0x69))
+    }
 
-    pub fn resume_all() {}
+    pub fn resume_all(&mut self) -> Result<(), SendError> {
+        self.send_msc_frame(Command::Resume, None, None)
+    }
 
-    fn send_msc_frame(&mut self, command: Command, cue_number: u8, list_entry: u8) -> Result<(), SendError> {
-        // TODO: Serialize cue_number into a sequence of bytes that the Express will accept
-        let cue_nums: Vec<u8> = cue_number.to_string().chars().map(|c| (0x30 + c.to_digit(10).unwrap()) as u8).collect();
+    pub fn fire_macro(&mut self, macro_number: u8) -> Result<(), SendError> {
+        self.midi_conn.send(&[
+            0xF0,
+            0x7F,
+            self.device_id,
+            0x01,
+            Command::Fire.value(),
+            macro_number,
+            0xF7,
+        ])
+    }
 
-        let bytes = [vec![0xF0, 0x7F, self.device_id, 0x01, command.value()], cue_nums, vec![0x00, list_entry, 0x00, 0xF7]].concat();
+    fn send_msc_frame(
+        &mut self,
+        command: Command,
+        fader_pair: Option<FaderPair>,
+        cue_number: Option<u16>,
+    ) -> Result<(), SendError> {
+        let mut cue_nums: Vec<u8> = vec![];
+        let mut fader_pair_bytes: Vec<u8> = vec![];
+
+        if let Some(cue_number) = cue_number {
+            cue_nums = cue_number
+                .to_string()
+                .chars()
+                .map(|c| (0x30 + c.to_digit(10).unwrap()) as u8)
+                .collect();
+            cue_nums.push(0x00);
+        }
+
+        if let Some(fader_pair) = fader_pair {
+            fader_pair_bytes = vec![fader_pair.value(), 0x00];
+        }
+
+        let bytes = [
+            vec![0xF0, 0x7F, self.device_id, 0x01, command.value()],
+            cue_nums,
+            fader_pair_bytes,
+            vec![0xF7],
+        ]
+        .concat();
         self.midi_conn.send(&bytes)
     }
 }
