aef53ac30a3f71cd3300308e603f3483d2bafaa7
Brenek Harrison <brenekharrison@gmail.com>
2023-07-12 18:31:55 -0600

    Add examples to public methods that didn't have one

--- src/msc.rs
+++ src/msc.rs
@@ -71,26 +71,96 @@ impl ConsoleMSC {
     }
 
     /// Stop the currently executing cue in a specific [FaderPair]
+    ///
+    /// ```rust
+    /// # use etc_express_midi::{MidiOutput, ConsoleMSC, FaderPair};
+    /// # let midi_client = MidiOutput::new("ETC MSC Example")?;
+    /// # let midi_ports = midi_client.ports();
+    /// # let midi_port_index = 0; // The index of the desired controller
+    /// # let midi_conn = midi_client.connect(&(midi_ports[midi_port_index]), "Example Output")?;
+    /// # let msc_device_id = 0;
+    /// # let mut express_console = ConsoleMSC::new(midi_conn, msc_device_id);
+    /// // ...
+    ///
+    /// express_console.stop(FaderPair::AB)?;
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn stop(&mut self, fader_pair: FaderPair) -> Result<(), Error> {
         self.send_msc_frame(Command::Stop, Some(fader_pair), Some(0x69))
     }
 
     /// Stop all currently executing cues
+    ///
+    /// ```rust
+    /// # use etc_express_midi::{MidiOutput, ConsoleMSC};
+    /// # let midi_client = MidiOutput::new("ETC MSC Example")?;
+    /// # let midi_ports = midi_client.ports();
+    /// # let midi_port_index = 0; // The index of the desired controller
+    /// # let midi_conn = midi_client.connect(&(midi_ports[midi_port_index]), "Example Output")?;
+    /// # let msc_device_id = 0;
+    /// # let mut express_console = ConsoleMSC::new(midi_conn, msc_device_id);
+    /// // ...
+    ///
+    /// express_console.stop_all()?;
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn stop_all(&mut self) -> Result<(), Error> {
         self.send_msc_frame(Command::Stop, None, None)
     }
 
     /// Resume the cue in a specific [FaderPair]
+    ///
+    /// ```rust
+    /// # use etc_express_midi::{MidiOutput, ConsoleMSC, FaderPair};
+    /// # let midi_client = MidiOutput::new("ETC MSC Example")?;
+    /// # let midi_ports = midi_client.ports();
+    /// # let midi_port_index = 0; // The index of the desired controller
+    /// # let midi_conn = midi_client.connect(&(midi_ports[midi_port_index]), "Example Output")?;
+    /// # let msc_device_id = 0;
+    /// # let mut express_console = ConsoleMSC::new(midi_conn, msc_device_id);
+    /// // ...
+    ///
+    /// express_console.resume(FaderPair::AB)?;
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn resume(&mut self, fader_pair: FaderPair) -> Result<(), Error> {
         self.send_msc_frame(Command::Resume, Some(fader_pair), Some(0x69))
     }
 
     /// Resume all cues currently stopped
+    ///
+    /// ```rust
+    /// # use etc_express_midi::{MidiOutput, ConsoleMSC};
+    /// # let midi_client = MidiOutput::new("ETC MSC Example")?;
+    /// # let midi_ports = midi_client.ports();
+    /// # let midi_port_index = 0; // The index of the desired controller
+    /// # let midi_conn = midi_client.connect(&(midi_ports[midi_port_index]), "Example Output")?;
+    /// # let msc_device_id = 0;
+    /// # let mut express_console = ConsoleMSC::new(midi_conn, msc_device_id);
+    /// // ...
+    ///
+    /// express_console.resume_all()?;
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn resume_all(&mut self) -> Result<(), Error> {
         self.send_msc_frame(Command::Resume, None, None)
     }
 
     /// Run a specific macro
+    ///
+    /// ```rust
+    /// # use etc_express_midi::{MidiOutput, ConsoleMSC};
+    /// # let midi_client = MidiOutput::new("ETC MSC Example")?;
+    /// # let midi_ports = midi_client.ports();
+    /// # let midi_port_index = 0; // The index of the desired controller
+    /// # let midi_conn = midi_client.connect(&(midi_ports[midi_port_index]), "Example Output")?;
+    /// # let msc_device_id = 0;
+    /// # let mut express_console = ConsoleMSC::new(midi_conn, msc_device_id);
+    /// // ...
+    ///
+    /// express_console.fire_macro(10)?;
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
     pub fn fire_macro(&mut self, macro_number: u8) -> Result<(), Error> {
         self.midi_conn
             .send(&[
